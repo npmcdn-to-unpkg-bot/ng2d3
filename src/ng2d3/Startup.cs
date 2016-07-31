d423e6af3a12af4224be4dc81b46d570971679d0
Oleg Kolpashchikov <oleg.kolpashchikov@kraftvaerk.com>
2016-07-31 16:10:38 +0300

    added MVC + empty controller

--- src/ng2d3/Startup.cs
+++ src/ng2d3/Startup.cs
@@ -10,28 +10,34 @@ using Microsoft.Extensions.Logging;
 
 namespace ng2d3
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc((mvcOptions) => {
+				var filters = mvcOptions.Filters;
+				
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+			//app.UseMvc((routeBuilder) => {
+			//	routeBuilder.Routes.Add(new Route)
+			//});
+
+			app.UseMvcWithDefaultRoute();
+
+		}
+	}
 }
