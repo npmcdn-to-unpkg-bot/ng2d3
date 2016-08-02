e4b28df117ac6907d92a4a12ab5ffe1ef737ac79
Oleg Kolpashchikov <oleg.kolpashchikov@kraftvaerk.com>
2016-08-02 22:51:58 +0300

    Sample 3 (Input-Attribute) from https://angular.io/resources/live-examples/attribute-directives/ts/plnkr.html

---
+++ src/ng2d3/Samples/03_InputAttribute/app/app.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+
+import { HighlightDirective } from './highlight.directive';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    directives: [HighlightDirective]
+})
+
+export class AppComponent { }
+
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/ng2d3/Samples/03_InputAttribute/app/highlight.directive.ts
@@ -0,0 +1,38 @@
+﻿import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+    selector: '[myHighlight]'
+})
+export class HighlightDirective {
+    private _defaultColor = 'red';
+    private el: HTMLElement;
+
+    constructor(el: ElementRef) { this.el = el.nativeElement; }
+
+    @Input() set defaultColor(colorName: string) {
+        this._defaultColor = colorName || this._defaultColor;
+    }
+
+    @Input('myHighlight') highlightColor: string;
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.highlight(this.highlightColor || this._defaultColor);
+    }
+    @HostListener('mouseleave') onMouseLeave() {
+        this.highlight(null);
+    }
+
+    private highlight(color: string) {
+        this.el.style.backgroundColor = color;
+    }
+}
+/*
+@Input() myHighlight: string;
+*/
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
